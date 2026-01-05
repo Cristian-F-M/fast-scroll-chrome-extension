ef5f5906fee57a4626f7940fd17adf14718db444
Cristian-F-M <cfmorales.diaz@gmail.com>
2026-01-05 15:39:32 -0500

    Add base path and assetsDir to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+	build: {
+		assetsDir: ''
+	},
+	base: '/popup/',
 	plugins: [react(), tailwindcss()]
 })
